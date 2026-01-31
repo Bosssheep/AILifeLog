a93e0d27c86330b9a0b2e3d0e0dfe4b5351a5037
“ChenHanhyi” <389137297@qq.com>
2026-01-31 19:31:19 +0700

    chore: 配置 GitLab Pages 自动部署

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // <--- 核心：确保打包后的资源引用路径为相对路径
+});
